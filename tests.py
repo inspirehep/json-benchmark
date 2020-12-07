37e9dcfad1a00426d44c775d982559c04463ffbc
pazembrz <zembrzuski.pawel@gmail.com>
2020-12-07 15:44:26 +0100

    Update benchmark code

--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from statistics import quantiles
+
 import csv
 
 import functools
@@ -11,22 +14,17 @@ import ujson
 
 import os
 
+jsons = [json, orjson, ujson, rapidjson, simplejson]
+
 
 def test_files(path):
     results = []
-    jsons=[json, orjson, ujson, rapidjson, simplejson]
     (dirpath, dirnames, filenames) = next(os.walk(path))
     for filename in filenames:
-        with open(f"{dirpath}/{filename}", 'r') as file:
-            data = file.read()
-            print(f"file: {filename} - loads, dumps")
-            results_partial = {"file": filename}
-            for json_lib in jsons:
-                json_load, json_dump = benchmark(data, json_lib)
-                print(f"{json_lib.__name__}: {json_load}, {json_dump}")
-                results_partial[f"{json_lib.__name__}_load"] = json_load
-                results_partial[f"{json_lib.__name__}_dump"] = json_dump
-            results.append(results_partial)
+        results.append(process_file(dirpath, filename))
+    #Not storing results anymore
+
+def store_results(results):
     with open("results.json", "w") as output:
         json.dump(results, output)
     with open('results.csv', 'w') as output:
@@ -36,10 +34,44 @@ def test_files(path):
             csv_file.writerow(row)
 
 
+def process_file(dirpath, filename):
+    with open(f"{dirpath}/{filename}", 'r') as file:
+        data_lines = file.readlines()
+        print(f"file: {filename}:")
+        partial_results = process_file_lines(filename, data_lines)
+
+        stats = produce_stats(partial_results)
+        print(stats)
+
+        return stats
+
+
+def produce_stats(partial_results):
+    summary = {}
+    for key, values in partial_results.items():
+        min_value = min(values)
+        max_value = max(values)
+        avg_value = sum(values) / len(values)
+        quants = quantiles(values, n=100)
+        summary[key] = {'min': min_value, 'max': max_value, 'avg': avg_value,
+                        'p5': quants[4], 'p25': quants[24], 'p50': quants[49], 'p75': quants[74],
+                        'p95': quants[94]}
+    return summary
+
+def process_file_lines(filename, data_lines):
+    results_partial = defaultdict(list)
+    for data in data_lines:
+        for json_lib in jsons:
+            json_load, json_dump = benchmark(data, json_lib)
+            results_partial[f"{json_lib.__name__}_load"].append(json_load)
+            results_partial[f"{json_lib.__name__}_dump"].append(json_dump)
+    return results_partial
+
+
 def benchmark(data, json_lib):
-    load_json = timeit.Timer(functools.partial(json_lib.loads, data), setup=f"import {json_lib.__name__}").timeit(100)
+    load_json = timeit.Timer(functools.partial(json_lib.loads, data), setup=f"import {json_lib.__name__}").timeit(5)
     json_data = json_lib.loads(data)
-    dump_json = timeit.Timer(functools.partial(json_lib.dumps, json_data), setup=f"import {json_lib.__name__}").timeit(100)
+    dump_json = timeit.Timer(functools.partial(json_lib.dumps, json_data), setup=f"import {json_lib.__name__}").timeit(5)
     return load_json, dump_json
 
 
